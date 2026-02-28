6ff73b0582095a64cc8701648c6af1bfe81366d0
Aadhar Chand <aadharchand01@gmail.com>
2026-02-28 16:37:28 +0545

    update code

--- src/models/auth/auth.repository.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "../../models/user.model";
-
-export const findByEmail = (email: string) => {
-  return User.findOne({ email });
-};
-
-export const createUser = (data: any) => {
-  return User.create(data);
-};
--- src/utils/jwt.ts
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const generateToken = (payload: object) => {
-  return jwt.sign(payload, process.env.JWT_SECRET as string, {
-    expiresIn: "1d",
-  });
-};
